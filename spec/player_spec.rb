4c66f156d480d97cd4fa84c95edf65bf5e149728
Will Gant <w_gant@yahoo.co.uk>
2017-07-20 16:58:41 +0000

    Alters player test to check for instiating with ten vessels

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,12 +1,13 @@
 require_relative '../app.rb'
 
 describe Player do
-    it 'starts the game with the right collection of vessels' do
-        player = Player.new
-        expect(player.vessels.length).to eq 10
-        expect(player.vessels.count { |vessel| vessel.type == :battleship }).to eq 1
-        expect(player.vessels.count { |vessel| vessel.type == :cruiser }).to eq 2
-        expect(player.vessels.count { |vessel| vessel.type == :destroyer }).to eq 3
-        expect(player.vessels.count { |vessel| vessel.type == :submarine }).to eq 4
+    before(:each) do
+        vessel = double("vessel")
+        allow(vessel).to receive(:new).and_return(vessel)
+    end
+    
+    it 'starts the game by instantiating ten vessels' do
+        player = Player.new(vessel)
+        expect(player.vessels).to receive(:new).exactly(10).times
     end
 end
