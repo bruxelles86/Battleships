c0016b382e2b20abe2686f92e436b54ccf2307e4
Will Gant <w_gant@yahoo.co.uk>
2017-07-22 08:04:06 +0000

    Adds extra test to show new_grid size matches arguments

--- spec/grid_builder_spec.rb
+++ spec/grid_builder_spec.rb
@@ -6,4 +6,10 @@ describe GridBuilder do
         grid = grid_builder.new_grid
         expect(grid.values.flatten.length).to eq 100
     end
+    
+    it 'generates a 12 x 12 grid if new_grid is passed 12 and 12 as arguments' do
+        grid_builder = GridBuilder.new
+        grid = grid_builder.new_grid(12, 12)
+        expect(grid.values.flatten.length).to eq 144
+    end
 end
