43f0e7548e1e11e167375000487bd2e2059764c7
Will Gant <w_gant@yahoo.co.uk>
2017-09-07 14:47:25 +0000

    Vessels have attribute for whether placed or not

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -5,5 +5,6 @@ class Vessel
         @type = vessel_type
         @vessel_lengths = { battleship: 4, cruiser: 3, destroyer: 2, submarine: 1 }
         @length = @vessel_lengths[vessel_type]
+        @placed = false
     end
 end
