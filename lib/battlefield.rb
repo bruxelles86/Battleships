4e28cc3c79189ba7e75e20d16f6c392fbe0a2523
Will Gant <w_gant@yahoo.co.uk>
2017-09-09 12:32:35 +0000

    adds battlefield predicate methods straight_line? and within_grid?

--- lib/battlefield.rb
+++ lib/battlefield.rb
@@ -12,4 +12,20 @@ class Battlefield
         #  if all criteria are met, sets right grid squares 'occupied' fields to true
 
     end
+    
+    def straight_line?(coord1, coord2)
+        return true if coord1[0] == coord2[0]
+        return true if coord1[1] == coord2[1]
+        return false
+    end
+    
+    def within_grid?(coord1, coord2, grid)
+        num1 = coord1.to_s.slice(1..-1).to_i
+        num2 = coord2.to_s.slice(1..-1).to_i
+        lines_exist = true if grid[(coord1[0].to_s)] && grid[(coord2[0].to_s)]
+        nums_above_1 = true if num1 >= 1 && num2 >= 1
+        squares_exist = true if nums_above_1 && grid[(coord1[0].to_s)][num1-1] && grid[(coord2[0].to_s)][num2-1]
+        return true if lines_exist && squares_exist
+        return false
+    end
 end
